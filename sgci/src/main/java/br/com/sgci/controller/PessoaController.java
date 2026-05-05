c240269331f160e6c1a15e6e66e98807d5f7c0a0
Mateus Maciel Azevedo <mateusmazevedo_@192.168.0.52>
2026-05-05 15:17:13 -0300

    feat: inclui liberação do CORS

--- sgci/src/main/java/br/com/sgci/controller/PessoaController.java
+++ sgci/src/main/java/br/com/sgci/controller/PessoaController.java
@@ -1,9 +1,8 @@
 package br.com.sgci.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +26,7 @@ import jakarta.validation.Valid;
 @Tag(name = "Pessoa::Pessoa")
 @RestController
 @RequestMapping("pessoas")
+@CrossOrigin(origins = "*")
 
 
 public class PessoaController {
